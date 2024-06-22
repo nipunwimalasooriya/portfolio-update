9bd4fbbf0234e3c66212c193282f56658ac3b1bf
Nipun Wimalasooriya <40894068+nipunwimalasooriya@users.noreply.github.com>
2024-06-22 13:15:51 +0530

    new sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import Hero from "./components/Hero"
+import styled from "styled-components"
+import Who from "./components/Who"
+import Works from "./components/Works"
+import Contact from "./components/Contact"
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`
+
+
+
 function App() {
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+     <Hero/>
+     <Who/>
+     <Works/>
+     <Contact/>
+    </Container>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Section = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+`
+
+const Hero = () => {
+    return(
+        <Section>Hero</Section>
+    )
+}
