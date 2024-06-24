fb5c68f20e6139dc5aa5c4537b8484a077009d66
Nipun Wimalasooriya <40894068+nipunwimalasooriya@users.noreply.github.com>
2024-06-24 13:17:15 +0530

    completed navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+
+`
+const Container = styled.div`
+    width: 1400px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 20px;
+
+`;
+
+const Links = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 30px;
+    `;
+
+const Logo= styled.img`
+    height: 30px;
+
+`;
+
+const List = styled.ul`
+    display: flex;
+    gap: 20px;
+    list-style: none;
+    `;
+
+const ListItem = styled.li`
+    cursor: pointer;
+
+`;
+
+const Icon = styled.img`
+    width: 20px;
+    cursor: pointer;
+
+
+`;
+
+const Icons = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+`;
+
+const Button = styled.button`
+    cursor: pointer;
+    width: 100px;
+    padding: 10px;
+    background-color: #643bd3;
+    color: #ffffff;
+    border: none;
+    border-radius: 5px;
+    font-weight: bold;
+
+`;
+
+const Navbar = () => {
+    return(
+        <Section>
+        <Container>
+            <Links>
+            <Logo src="./img/logo white.png"/>
+            <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Studio</ListItem>
+                <ListItem>Works</ListItem>
+                <ListItem>Contact</ListItem>
+            </List>
+            </Links>
+            <Icons>
+                <Icon src = "./img/search.png"/>
+                <Button>Hire Now</Button>
+            </Icons>
+        </Container>
+        </Section>
+    )
+}
+
+export default Navbar
