9dc1c69440a7a653cdfab00be442eceb228755b5
Nipun Wimalasooriya <40894068+nipunwimalasooriya@users.noreply.github.com>
2024-06-26 14:20:27 +0530

    map section without contact form functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,99 @@
 import React from "react";
 import styled from "styled-components";
+import Map from "./Map";
 
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+`;
+
+const Titile = styled.h1`
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  width: 500px;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: #c9c9c9;
+  border: none;
+  border-radius: 5px;
+
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #c9c9c9;
+
+`;
+
+const Button = styled.button`
+    cursor: pointer;
+    width: 100px;
+    padding: 10px;
+    background-color: #643bd3;
+    color: #ffffff;
+    border: none;
+    border-radius: 5px;
+    font-weight: bold;
+    transition: color 0.3s ease-in, background-color 0.3s ease-in, box-shadow 0.3s ease-in;
+
+    &:hover {
+        color: #643bd3;
+        background-color: #ffffff;
+        box-shadow: 0 5px 15px rgba(145, 92, 182, .4);
+    }
+
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Contact = () => {
     return(
-        <Section>Contact</Section>
+        <Section>
+          <Container>
+            <Left>
+              <Form>
+                <Titile>Contact Me</Titile>
+                <Input placeholder="Name"/>
+                <Input placeholder="Email"/>
+                <TextArea placeholder="Write Your Message" rows={10}/>
+                <Button>Send</Button>
+              </Form>
+            </Left>
+            <Right>
+              <Map/>
+            </Right>
+          </Container>
+        </Section>
     )
 }
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,16 +1,96 @@
 import React from "react";
 import styled from "styled-components";
 
+const data = [
+  "Web Design",
+  "Development",
+  "Video Editing",
+  "Photo Editing",
+  "Social Media",
+];
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const ListItem = styled.li`
+  font-size: 90px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+  position: relative;
+
+  &::before {
+    content: "${(props) => props.text}";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0%;
+    color: white;
+    overflow: hidden;
+    white-space: nowrap;
+    transition: width 0.5s ease;
+  }
+
+  &:hover::before {
+    width: 100%;
+  }
+
+  @media only screen and (max-width: 768px) {
+    font-size: 24px;
+    color: white;
+    -webkit-text-stroke: 0px;
+
+    &::before {
+      display: none;
+    }
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Works = () => {
-    return(
-        <Section>Works</Section>
-    )
-}
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item} text={item}>
+                {item}
+              </ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
+};
 
 export default Works;
