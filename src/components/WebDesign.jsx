75b4b572d1ef6e0bc6a0285124e29574c436c17f
Nipun Wimalasooriya <40894068+nipunwimalasooriya@users.noreply.github.com>
2024-07-01 02:37:16 +0530

    added new rotation

--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -1,10 +1,16 @@
+import { OrbitControls, Stage } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import React from 'react';
+import Codingold from './Codingold';
 
 const WebDesign = () => {
     return (
-        <div>
-            
-        </div>
+        <Canvas camera = {{fov:80, position:[5,5,0]}}>
+            <Stage environment={"city"} intensity={0.6}>
+            <Codingold/>
+            </Stage>
+            <OrbitControls enableZoom = {false} autoRotate autoRotateSpeed={1}/>
+        </Canvas>
     );
 };
 
