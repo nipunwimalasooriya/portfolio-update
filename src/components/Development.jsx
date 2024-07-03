a1c533f2de7d9fcc2e942ef61016441dc2035f83
Nipun Wimalasooriya <40894068+nipunwimalasooriya@users.noreply.github.com>
2024-07-03 21:41:14 +0530

    updated 3d models

--- src/components/Development.jsx
+++ src/components/Development.jsx
@@ -1,10 +1,16 @@
+import { OrbitControls, Stage } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import React from 'react';
+import Atom from './Atom';
 
 const Development = () => {
     return (
-        <div>
-            
-        </div>
+        <Canvas>
+            <Stage environment={"city"} intensity={0.6}>
+            <Atom/>
+            </Stage>
+            <OrbitControls enableZoom = {false} autoRotate autoRotateSpeed={1.3}/>
+        </Canvas>
     );
 };
 
--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -1,15 +1,15 @@
 import { OrbitControls, Stage } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import React from 'react';
-import Codingold from './Codingold';
+import Mac from './Mac';
 
 const WebDesign = () => {
     return (
-        <Canvas camera = {{fov:80, position:[5,5,0]}}>
+        <Canvas>
             <Stage environment={"city"} intensity={0.6}>
-            <Codingold/>
+            <Mac/>
             </Stage>
-            <OrbitControls enableZoom = {false} autoRotate autoRotateSpeed={1}/>
+            <OrbitControls enableZoom = {false} autoRotate autoRotateSpeed={4}/>
         </Canvas>
     );
 };
